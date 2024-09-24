de9dcd7d2d15e574d5431a965b33299c98452348
20110032 <lipy.sh@outlook.com>
2024-09-24 22:06:44 +0800

    2024-09-24分支master自动提交:整理了后台接口，将git的相关配置整理到git.json中，允许对接多个gitlab服务器

--- push.sh
+++ push.sh
@@ -6,8 +6,10 @@ path=$(cd "$(dirname "$0")"; pwd)
 # 获取命令行参数作为提交信息，如果没有则默认为空
 commit="${1:+:$1}"
 
+# 获取分支信息
+branch=$(git branch | grep \* | cut -d ' ' -f2)
 # 构建git命令
-cmd="cd $path && git pull && git add . && git add * && git commit -m \"$(date +%Y-%m-%d)自动提交$commit\" && git push"
+cmd="cd $path && git pull && git add -f . && git add -f * && git commit -m \"$(date +%Y-%m-%d)分支$branch自动提交$commit\" && git push"
 
 # 打印命令
 echo "$cmd"
