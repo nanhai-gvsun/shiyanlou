c448f40418c31cbd4c9a511f19c2e3acfbf345f6
20110032 <lipy.sh@outlook.com>
2024-10-08 11:06:50 +0800

    2024-10-08分支master自动提交

--- publish_shiyanshi.sh
+++ publish_shiyanshi.sh
@@ -45,20 +45,7 @@ create_symlink() {
 
 # 如果分支 'web' 已克隆，则创建符号链接
 if [ -d "$DIR/web" ]; then
-<<<<<<< HEAD
   create_symlink "$DIR/web" /etc/nginx/html
-=======
-  if [ -d "/data/data/com.termux/files/usr" ]; then
-    ln -sf "$DIR/web" /data/data/com.termux/files/usr/etc/nginx/html
-  else
-    ln -sf "$DIR/web" /etc/nginx/html
-  fi
-  if [ $? -eq 0 ]; then
-    echo "Symbolic link created: /etc/nginx/html -> $DIR/web"
-  else
-    echo "Failed to create symbolic link."
-  fi
->>>>>>> e3e49c76ceba44a087b319f39e0ae5c3efb35af7
 fi
 
 if [ -d "$DIR/doc" ]; then
@@ -69,36 +56,9 @@ if [ -d "$DIR/gsiot" ]; then
   create_symlink "$DIR/gsiot" "$DIR/sse-server/gsiot"
 fi
 
-<<<<<<< HEAD
 if [ -d "$DIR/sse-server" ]; then
   create_symlink "$DIR/sse-server/etc" /etc/sse-server
   create_symlink "$DIR/sse-server/etc/nginx/nginx.conf" /etc/nginx/nginx.conf
   create_symlink "$DIR/sse-server/etc/nginx/cakey.pem" /etc/nginx/cakey.pem
   create_symlink "$DIR/sse-server/etc/nginx/cacerts.pem" /etc/nginx/cacerts.pem
-=======
-# 检查是否在 Termux 环境中运行
-if [ -d "/data/data/com.termux/files/usr" ]; then
-  USR="/data/data/com.termux/files/usr"
-  if [ -d "$DIR/sse-server" ]; then
-    ln -sf "$DIR/sse-server/etc/nginx/nginx.conf" "$USR/etc/nginx/nginx.conf"
-    ln -sf "$DIR/sse-server/etc/nginx/cakey.pem" "$USR/etc/nginx/cakey.pem"
-    ln -sf "$DIR/sse-server/etc/nginx/cacerts.pem" "$USR/etc/nginx/cacerts.pem"
-    if [ $? -eq 0 ]; then
-      echo "Symbolic links created for Termux environment."
-    else
-      echo "Failed to create symbolic links for Termux environment."
-    fi
-  fi
-else
-  if [ -d "$DIR/sse-server" ]; then
-    ln -sf "$DIR/sse-server/etc/nginx/nginx.conf" /etc/nginx/nginx.conf
-    ln -sf "$DIR/sse-server/etc/nginx/cakey.pem" /etc/nginx/cakey.pem
-    ln -sf "$DIR/sse-server/etc/nginx/cacerts.pem" /etc/nginx/cacerts.pem
-    if [ $? -eq 0 ]; then
-      echo "Symbolic links created for standard environment."
-    else
-      echo "Failed to create symbolic links for standard environment."
-    fi
-  fi
->>>>>>> e3e49c76ceba44a087b319f39e0ae5c3efb35af7
 fi
